65f456a0882421e33c3288690f3fc205201480c7
Seth Pollack <seth@sethpollack.net>
2017-09-26 14:52:11 -0400

    add ecr support

--- .drone.sh
+++ .drone.sh
@@ -19,4 +19,6 @@ GOOS=linux GOARCH=arm64 CGO_ENABLED=0         go build -a -tags netgo -o release
 GOOS=linux GOARCH=arm   CGO_ENABLED=0 GOARM=7 go build -a -tags netgo -o release/linux/arm/drone-docker-gcr   github.com/drone-plugins/drone-docker/cmd/drone-docker-gcr
 
 # build the ecr binary
-# todo
+GOOS=linux GOARCH=amd64 CGO_ENABLED=0         go build -a -tags netgo -o release/linux/amd64/drone-docker-ecr github.com/drone-plugins/drone-docker/cmd/drone-docker-ecr
+GOOS=linux GOARCH=arm64 CGO_ENABLED=0         go build -a -tags netgo -o release/linux/arm64/drone-docker-ecr github.com/drone-plugins/drone-docker/cmd/drone-docker-ecr
+GOOS=linux GOARCH=arm   CGO_ENABLED=0 GOARM=7 go build -a -tags netgo -o release/linux/arm/drone-docker-ecr   github.com/drone-plugins/drone-docker/cmd/drone-docker-ecr
