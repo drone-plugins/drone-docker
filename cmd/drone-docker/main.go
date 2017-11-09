c2b4c08da4fb1246cc51a4d954b1620150088018
Brad Rydzewski <brad.rydzewski@gmail.com>
2017-11-09 13:41:30 -0800

    use BoolTFlag for purge flag

--- cmd/drone-docker/main.go
+++ cmd/drone-docker/main.go
@@ -212,7 +212,7 @@ func main() {
 func run(c *cli.Context) error {
 	plugin := docker.Plugin{
 		Dryrun:  c.Bool("dry-run"),
-		Cleanup: c.Bool("purge"),
+		Cleanup: c.BoolTFlag("docker.purge"),
 		Login: docker.Login{
 			Registry: c.String("docker.registry"),
 			Username: c.String("docker.username"),
