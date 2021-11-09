6975739753e2b54b1c4918398bd8b111b3b8943c
Eoin McAfee <eoin.mcafee@harness.io>
2021-11-09 15:37:49 +0000

    fix window build

--- daemon_win.go
+++ daemon_win.go
@@ -5,6 +5,7 @@ package docker
 const dockerExe = "C:\\bin\\docker.exe"
 const dockerdExe = ""
 const dockerHome = "C:\\ProgramData\\docker\\"
+const bashExe = "/bin/sh"
 
 func (p Plugin) startDaemon() {
 	// this is a no-op on windows
