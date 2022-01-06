c898b40dd6f3c8fe5199e219949df972cbc08108
Shubham Agrawal <shubham.agrawal@harness.io>
2022-01-06 20:27:31 +0530

    Fix windows pipeline

--- scripts/windows/latest.ps1
+++ scripts/windows/latest.ps1
@@ -17,14 +17,19 @@ if (-not (Test-Path env:REGISTRY)) {
 echo $env:GOOS
 echo $env:GOARCH
 echo $env:VERSION
+echo $env:REGISTRY
 
 # build the binary
+Write-Host "+ go build -o release/windows/amd64/drone-$env:REGISTRY.exe";
 go build -o release/windows/amd64/drone-$env:REGISTRY.exe
 
 # build and publish the docker image
 docker login -u $env:USERNAME -p $env:PASSWORD
+Write-Host "+ docker build -f docker/$env:REGISTRY/Dockerfile.windows.amd64.$env:VERSION -t plugins/$env:REGISTRY:windows-$env:VERSION-amd64 .";
 docker build -f docker/$env:REGISTRY/Dockerfile.windows.amd64.$env:VERSION -t plugins/$env:REGISTRY:windows-$env:VERSION-amd64 .
+Write-Host "+ docker push plugins/$env:REGISTRY:windows-$env:VERSION-amd64"
 docker push plugins/$env:REGISTRY:windows-$env:VERSION-amd64
 
 # remove images from local cache
+Write-Host "+ docker rmi plugins/$env:REGISTRY:windows-$env:VERSION-amd64"
 docker rmi plugins/$env:REGISTRY:windows-$env:VERSION-amd64
