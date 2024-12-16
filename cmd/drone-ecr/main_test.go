6ba07f42e4a10dff26cb31b7c73be2ad2dd9e69b
dvan <dvan@squarespace.com>
2024-12-16 12:08:45 -0500

    add tests to ensure mutable/immutable strings are correct

--- cmd/drone-ecr/main_test.go
+++ cmd/drone-ecr/main_test.go
@@ -1,6 +1,10 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/aws/aws-sdk-go/service/ecr"
+)
 
 func TestTrimHostname(t *testing.T) {
 	registry := "000000000000.dkr.ecr.us-east-1.amazonaws.com"
@@ -16,5 +20,33 @@ func TestTrimHostname(t *testing.T) {
 		if splitName != name {
 			t.Errorf("%s is not equal to %s.", splitName, name)
 		}
+	}
+}
+
+func TestGetTagImmutableString(t *testing.T) {
+	testCases := []struct {
+		name         string
+		tagImmutable bool
+		expected     string
+	}{
+		{
+			name:         "mutable",
+			tagImmutable: false,
+			expected:     ecr.ImageTagMutabilityMutable,
+		},
+		{
+			name:         "immutable",
+			tagImmutable: true,
+			expected:     ecr.ImageTagMutabilityImmutable,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			actual := getTagImmutableString(tc.tagImmutable)
+			if actual != tc.expected {
+				t.Errorf("expected: %s, actual: %s", tc.expected, actual)
+			}
+		})
 	}
 }
